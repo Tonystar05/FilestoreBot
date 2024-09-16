c8b151fdc91c11e5faa41b087245c8a1a85e5ad7
Tonystar05 <yedhuku2005@gmail.com>
2024-09-16 17:33:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
 import os
+from os import environ
 import logging
 from logging.handlers import RotatingFileHandler
-
 
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6722606812:AAEdb3pmMLJhK_qHhJsA5eT03bKtgl2SrjQ")
 API_ID = int(os.environ.get("API_ID", "15529802"))
 API_HASH = os.environ.get("API_HASH", "92bcb6aa798a6f1feadbc917fccb54d3")
+
+from os import environ
+
+API = environ.get("API", "") # shortlink api
+URL = environ.get("URL", "") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
 
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "821215952"))
