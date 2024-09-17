15d400c90171fd6752308064ac69c0723e6718b9
Tonystar05 <yedhuku2005@gmail.com>
2024-09-17 16:04:53 +0530

    Update start.py
    Uii

--- plugins/start.py
+++ plugins/start.py
@@ -54,66 +54,66 @@ async def start_command(client: Client, message: Message):
     
         elif len(message.text) > 7 and verify_status['is_verified']:
             try:
-               base64_string = message.text.split(" ", 1)[1]
+                base64_string = message.text.split(" ", 1)[1]
             except:
                 return
             _string = await decode(base64_string)
             argument = _string.split("-")
             if len(argument) == 3:
                 try:
-                start = int(int(argument[1]) / abs(client.db_channel.id))
-                end = int(int(argument[2]) / abs(client.db_channel.id))
-            except:
-                return
-            if start <= end:
-                ids = range(start,end+1)
-            else:
-                ids = []
-                i = start
-                while True:
-                    ids.append(i)
-                    i -= 1
-                    if i < end:
-                        break
-        elif len(argument) == 2:
-            try:
-                ids = [int(int(argument[1]) / abs(client.db_channel.id))]
-            except:
-                return
-        temp_msg = await message.reply("Please Wait...")
-        try:
-            messages = await get_messages(client, ids)
-        except:
-            await message.reply_text("Something Went Wrong..!")
-            return
-        await temp_msg.delete()
+                    start = int(int(argument[1]) / abs(client.db_channel.id))
+                    end = int(int(argument[2]) / abs(client.db_channel.id))
+                except:
+                    return
+                if start <= end:
+                    ids = range(start, end+1)
+                else:
+                    ids = []
+                    i = start
+                    while True:
+                        ids.append(i)
+                        i -= 1
+                        if i < end:
+                            break
+           elif len(argument) == 2:
+              try:
+                  ids = [int(int(argument[1]) / abs(client.db_channel.id))]
+              except:
+                  return
+           temp_msg = await message.reply("Please Wait...")
+           try:
+               messages = await get_messages(client, ids)
+           except:
+               await message.reply_text("Something Went Wrong..!")
+               return
+           await temp_msg.delete()
     
         madflix_msgs = [] # List to keep track of sent messages
 
-        for msg in messages:
+           for msg in messages:
 
-            if bool(CUSTOM_CAPTION) & bool(msg.document):
-                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
-            else:
-                caption = "" if not msg.caption else msg.caption.html
+               if bool(CUSTOM_CAPTION) & bool(msg.document):
+                   caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
+               else:
+                   caption = "" if not msg.caption else msg.caption.html
 
-            if DISABLE_CHANNEL_BUTTON:
-                reply_markup = msg.reply_markup
-            else:
-                reply_markup = None
+               if DISABLE_CHANNEL_BUTTON:
+                   reply_markup = msg.reply_markup
+               else:
+                   reply_markup = None
 
-            try:
-                madflix_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                # await asyncio.sleep(0.5)
-                madflix_msgs.append(madflix_msg)
+               try:
+                   madflix_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                 # await asyncio.sleep(0.5)
+                   madflix_msgs.append(madflix_msg)
                 
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                madflix_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                madflix_msgs.append(madflix_msg)
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    madflix_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                    madflix_msgs.append(madflix_msg)
                 
-            except:
-                pass
+                except:
+                    pass
 
 
         k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗️</b>\n\nThis Video / File Will Be Deleted In {file_auto_delete} (Due To Copyright Issues).\n\n📌 Please Forward This Video / File To Somewhere Else And Start Downloading There.")
